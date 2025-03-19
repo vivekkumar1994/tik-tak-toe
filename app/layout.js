3c30da1db3c8c94632191585bb3f1b403323cbc6
vivek kumar <viveksnh933@gmail.com>
2025-03-19 23:05:06 +0530

    new code updated

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from 'react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+          <Suspense fallback={<div>Loading...</div>}></Suspense>
         {children}
       </body>
     </html>
